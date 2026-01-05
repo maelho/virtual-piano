11a621c68f7dba2711919c0c9a5fd3f898e0fe60
maelho <51026438+maelho@users.noreply.github.com>
2026-01-05 13:58:17 -0400

    fix verbatimModuleSyntax soundfont

--- src/components/keyboard/with-instrument.tsx
+++ src/components/keyboard/with-instrument.tsx
@@ -1,4 +1,4 @@
-import { SoundfontProvider } from '../../adapters/soundfont'
+import { SoundfontProvider } from '../../adapters/soundfont/index.ts'
 import { useInstrument } from '../../state/instrument'
 import { useAudioContext } from '../audio-context-provider'
 import { Keyboard, type KeyboardProps } from './keyboard'
