2ae5a4800413e1651b7ab5c474c8045ffe075738
maelho <51026438+maelho@users.noreply.github.com>
2026-01-04 21:29:19 -0400

    change the keys - White keys (natural notes): q, w, e, r, t, y, u, i, o, p, z, x, c, v - Black keys (sharps/flats): 2, 3, 5, 6, 7, 9, 0, s, d, f

--- src/domain/keyboard.ts
+++ src/domain/keyboard.ts
@@ -2,12 +2,12 @@ import type { OctaveIndex, PitchIndex } from './note'
 
 export type Key = string
 export type Keys = Key[]
+export const LOWER_OCTAVE: Keys = ['q', '2', 'w', '3', 'e', 'r', '5', 't', '6', 'y', '7', 'u']
+export const UPPER_OCTAVE: Keys = ['i', '9', 'o', '0', 'p', 'z', 's', 'x', 'd', 'c', 'f', 'v']
 
-export const TOP_ROW: Keys = Array.from('q2w3er5t6y7u')
-export const BOTTOM_ROW: Keys = Array.from('zsxdcvgbhnjm')
 export const CHANGE_ROW_AT: OctaveIndex = 5
 
 export function selectKey(octave: OctaveIndex, index: PitchIndex): Key {
-  const keysRow = octave < CHANGE_ROW_AT ? TOP_ROW : BOTTOM_ROW
+  const keysRow = octave < CHANGE_ROW_AT ? LOWER_OCTAVE : UPPER_OCTAVE
   return keysRow[index]
 }
